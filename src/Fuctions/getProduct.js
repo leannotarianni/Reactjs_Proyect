28f3c54f3430f2a9fe0b504ba093dea867d690b7
Leandro Notarianni <leannotarianni@gmail.com>
2022-06-17 11:59:05 -0300

    getproduct update

--- src/Fuctions/getProduct.js
+++ src/Fuctions/getProduct.js
@@ -1,10 +1,10 @@
-import { doc,getDocs } from "firebase/firestore";
+import { doc,getDoc } from "firebase/firestore";
 import db from "../data/firebaseconfig";
 
 const getProduct = async (id) =>{
     console.log("id producto", id)
     const docRef = doc(db, "products", id)
-    const docSnaptshop = await getDocs(docRef);
+    const docSnaptshop = await getDoc(docRef);
     /* console.log("productos firbase",productSnapshot) */
     let product = docSnaptshop.data()
     product.id = docSnaptshop.id
