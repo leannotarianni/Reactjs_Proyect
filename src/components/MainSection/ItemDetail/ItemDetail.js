bdd6f55928c3aa2a4e1323fbcb902beb2bda00a0
Leandro Notarianni <leannotarianni@gmail.com>
2022-09-22 19:52:56 -0300

    item detail update

--- src/components/MainSection/ItemDetail/ItemDetail.js
+++ src/components/MainSection/ItemDetail/ItemDetail.js
@@ -1,5 +1,4 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
 //Components
 import CartContext from '../../../Context/CartContext'
 import ItemCount from "../ItemCount/ItemCount"
@@ -19,22 +18,19 @@ const ItemDetail =({data})=> {
 
     return(
             <Grid container>
-                <Grid container sx={{margin:3}} className="item-detail-card" >
+                <Grid container className="item-detail-card" >
                 <Grid item xs={12} sm={6} className='item-detail-img' >
                     <div>
-                        <p>{title}</p>
+                        <p className='top-tittle'>{title}</p>
                         <img src={`/${image}`}  alt={`./${title}`}/>  
                     </div> 
                 </Grid>
                 <Grid item xs={12} sm={6} className="item-detail-descripcion" >    
                     <div>
-                        <p>{title}</p>
+                        <p className='buttom-tittle'>{title}</p>
                         <p>${price} </p>
                         <ItemCount  data={data} onAdd={onAdd} 
                         />  
-                        <button>
-                            <Link to='/Cart'>View Cart</Link>
-                        </button>
                     </div>
                     
                 </Grid>
